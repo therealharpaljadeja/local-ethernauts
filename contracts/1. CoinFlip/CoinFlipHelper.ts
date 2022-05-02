e5cae3b9c588ed6ef872e9a901bdf40cb6797227
therealharpaljadeja <jadejaharpal14@gmail.com>
2022-05-02 16:31:50 +0530

    coinflip challenge completed

--- contracts/1. CoinFlip/CoinFlipHelper.ts
+++ contracts/1. CoinFlip/CoinFlipHelper.ts
@@ -1,8 +1,16 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
+const oneMinute = 60;
+
 const helper = async (victim: any, attacker: any) => {
   // add code here that will help you pass the test
+    for(let i = 0; i < 10; i++) {
+        await attacker.hackContract();
+        // next block
+        await ethers.provider.send('evm_increaseTime', [oneMinute]);
+        await ethers.provider.send('evm_mine', []);
+    }
 };
 
 export default helper;
